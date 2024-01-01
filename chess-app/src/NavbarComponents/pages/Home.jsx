0b531d546483d245314746dd20fec26adcb72863
okio16 <87910665+okio16@users.noreply.github.com>
2024-01-02 00:05:42 +0200

    login
    home jsx login

--- chess-app/src/NavbarComponents/pages/Home.jsx
+++ chess-app/src/NavbarComponents/pages/Home.jsx
@@ -1,5 +1,25 @@
 import React from "react";
-
+import "./CssForThePages/Home.css"
 export const Home = () => {
-    return <div>Home</div>
+
+  
+
+     return (
+        <div className="jc"> 
+            <div className="jc1">
+                <form className='jic' action=''>
+                    <div className='mbt'>
+                        <label  htmlFor="email">Email   </label>
+                        <input className='mbtc' id='mbtc1' type="email" placeholder ="Enter Email"  />
+                    </div>
+                    <div className='mbt'>
+                        <label  htmlFor="password">Password   </label>
+                        <input className='mbtc' type="password" placeholder ="Enter Password" />
+                    </div>
+                    <button className='success'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
 }
+
